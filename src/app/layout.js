b7f1a8f754b60ce633aeda1201270e7ad66fef12
dhruvmisra <dhruvmisra@live.com>
2023-12-25 01:46:22 +0530

    Add global styles with fictoan-base

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,21 @@
 import { Inter } from "next/font/google";
 import { ContentWrapper, SidebarWrapper } from "fictoan-react";
-import "../styles/theme-dark.css";
-import "../styles/common.css";
+import "../styles/globals.css";
 
-const inter = Inter({ subsets : ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title       : "FICTOAN docs",
-    description : "Documentation for the FICTOAN Framework",
+    title: "FICTOAN docs",
+    description: "Documentation for the FICTOAN Framework",
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-        <body>
-        <SidebarWrapper></SidebarWrapper>
-        <ContentWrapper>
-            {children}
-        </ContentWrapper>
-        </body>
+            <body>
+                <SidebarWrapper></SidebarWrapper>
+                <ContentWrapper>{children}</ContentWrapper>
+            </body>
         </html>
     );
 }
