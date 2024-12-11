88d1ce1b243a96e97d4975dc878d7c82d0341f28
Sujan Sundareswaran <sujan.sundareswaran@gmail.com>
2024-12-11 11:28:20 +0530

    Rename Form Wrapper page to Form

--- src/app/components/form/page.jsx
+++ src/app/components/form/page.jsx
@@ -1,10 +1,10 @@
 import FormWrapperDocs from "./page.client";
 
 export const metadata = {
-    title       : "Form wrapper component — Fictoan UI",
-    description : "A parent wrapper for all form elements, used to space them evenly",
+    title       : "Form component — Fictoan UI",
+    description : "A parent for all form elements, used to space them evenly",
     openGraph   : {
-        title       : "Form wrapper component — Fictoan UI",
+        title       : "Form component — Fictoan UI",
         description : "A parent wrapper for all form elements, used to space them evenly",
         url         : "https://fictoan.io/components/form-wrapper",
         siteName    : "Fictoan UI",
@@ -13,7 +13,7 @@ export const metadata = {
                 url    : "https://fictoan.io/components/option-card/opengraph-image",
                 width  : 1200,
                 height : 630,
-                alt    : "Form wrapper component — Fictoan UI",
+                alt    : "Form component — Fictoan UI",
             },
         ],
         locale      : "en_US",
@@ -21,7 +21,7 @@ export const metadata = {
     },
     twitter     : {
         card        : "summary_large_image",
-        title       : "Form wrapper component — Fictoan UI",
+        title       : "Form component — Fictoan UI",
         description : "A parent wrapper for all form elements, used to space them evenly",
         images      : ["https://fictoan.io/components/option-card/opengraph-image"],
     },
